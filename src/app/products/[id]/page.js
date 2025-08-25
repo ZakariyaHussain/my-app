ddd6097d7c486888f2a85a43e7a59a0ae58764da
Zakariya Hussian <zakariyahussain1989@gmail.com>
2025-08-25 10:24:40 +0600

    a tag change to link

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,35 +1,4 @@
-// import products from "../../data/products";
-// import Link from "next/link";
-
-// export default function ProductDetails({ params }) {
-//   const { id } = params; // Next.js passes the [id] param
-//   const product = products.find((p) => p.id === parseInt(id));
-
-//   if (!product) {
-//     return (
-//       <div className="max-w-3xl mx-auto px-6 py-10 text-center">
-//         <h1 className="text-2xl font-bold mb-4">Product Not Found</h1>
-//         <Link href="/products" className="text-blue-600 hover:underline">
-//           Back to Products
-//         </Link>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className="max-w-3xl mx-auto px-6 py-10">
-//       <h1 className="text-3xl font-bold mb-6">{product.name}</h1>
-//       <p className="text-gray-700 mb-4">{product.description}</p>
-//       <p className="text-xl font-bold mb-6">${product.price.toFixed(2)}</p>
-//       <Link
-//         href="/products"
-//         className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-500 transition"
-//       >
-//         Back to Products
-//       </Link>
-//     </div>
-//   );
-// }
+import Link from "next/link";
 
 export default async function ProductDetails({ params }) {
   const { id } = params;
@@ -59,12 +28,12 @@ export default async function ProductDetails({ params }) {
       />
       <p className="text-gray-700 mb-4">{product.description}</p>
       <p className="text-xl font-bold mb-6">${product.price}</p>
-      <a
+      <Link
         href="/products"
         className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-500 transition"
       >
         Back to Products
-      </a>
+      </Link>
     </div>
   );
 }
